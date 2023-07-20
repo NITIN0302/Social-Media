3619d6ceaccf3ab1a1b12cea9877493096cb4de8
Nitin srivastva <nitinsrivastava2all@gmail.com>
2023-07-20 19:59:32 +0530

    FireBase Authentication

---
+++ src/Navbar.tsx
@@ -0,0 +1,33 @@
+import {Link} from "react-router-dom";
+import {auth} from "./config/firebase";
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { signOut } from "firebase/auth"
+import "./Navbar.css";
+
+export const Navbar = () =>{
+
+    const [user] = useAuthState(auth);
+
+    const signUserOut = async () =>{
+        await signOut(auth);
+    }
+
+    return (
+        <div className="Navbar">
+            <div> 
+                <Link to="/" className="link" style={{color:"white"}}>Home</Link>
+                <Link to='/login' className="link" style={{color:"white"}}> Login</Link>
+            </div>
+            <div className="info">
+                {/* checking the condition for img shows if user ise login */}
+                {user && (
+                    <>
+                    {/* <p className="log">{user?.displayName} </p> */}
+                    <img className="log" src={user?.photoURL || ""}/>
+                    <button className="log" onClick={signUserOut}>Log Out</button>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEWgaJJ5lSruFAMJo2-n1OSOpWyEUaQUc",
+  authDomain: "social-media-6fac7.firebaseapp.com",
+  projectId: "social-media-6fac7",
+  storageBucket: "social-media-6fac7.appspot.com",
+  messagingSenderId: "345792921300",
+  appId: "1:345792921300:web:1654fbe15603735ba54541"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
